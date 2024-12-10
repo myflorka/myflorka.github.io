5055d07a2e0f3493b486c51f0a5f8dc0b3d15bed
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-10 19:45:39 +0700

    oke cip

--- js/content/inputclient.js
+++ js/content/inputclient.js
@@ -1,3 +1,13 @@
+import {getValue} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/element.js";
+
 export function inputClient(){
+    const datapelanggan={
+        namapelanggan:getValue('namapelanggan'),
+        nomorpelanggan:getValue('nomorpelanggan'),
+        namapasangan:getValue('namapasangan'),
+        lokasi:getValue('lokasi'),
+        tanggal:getValue('tanggal')
+    }
+    console.log(datapelanggan); 
     
 }
