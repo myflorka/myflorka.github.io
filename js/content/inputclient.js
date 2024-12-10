56c04a24baf895089e110f639ab94e5ffd32a1d2
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-10 19:52:35 +0700

    benerin aksi submit

--- js/content/inputclient.js
+++ js/content/inputclient.js
@@ -1,6 +1,11 @@
-import {getValue} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/element.js";
+import {getValue,onClick} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/element.js";
 
 export function inputClient(){
+    onClick('tombolsubmitclient',aksiSubmit);
+    
+}
+
+function aksiSubmit(){
     const datapelanggan={
         namapelanggan:getValue('namapelanggan'),
         nomorpelanggan:getValue('nomorpelanggan'),
@@ -9,5 +14,4 @@ export function inputClient(){
         tanggal:getValue('tanggal')
     }
     console.log(datapelanggan); 
-    
 }
