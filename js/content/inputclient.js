adf2494bc52aaf7f3eb25d52e432eb071b1109e1
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-10 20:31:55 +0700

    disabled

--- js/content/inputclient.js
+++ js/content/inputclient.js
@@ -5,8 +5,24 @@ import {validatePhoneNumber} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8
 export function inputClient(){
     onClick('tombolsubmitclient',aksiSubmit);
     onInput('nomorpelanggan',validatePhoneNumber);
+    //check kelengkapan
+    onInput('namapelanggan',checkInputs);
+    onInput('nomorpelanggan',checkInputs);
+    onInput('namapasangan',checkInputs);
+    onInput('lokasi',checkInputs);
+    onInput('tanggal',checkInputs);
 }
 
+
+function checkInputs() {
+    if (getValue('namapelanggan').trim() !== '' && getValue('nomorpelanggan').trim() !== '' 
+    && getValue('namapasangan').trim() !== '' && getValue('lokasi').trim() !== '' 
+    && getValue('tanggal').trim() !== '') {
+      container('tombolsubmitclient').disabled = false; // Aktifkan tombol
+    } else {
+      container('tombolsubmitclient').disabled = true; // Nonaktifkan tombol
+    }
+  }
 
 function aksiSubmit(){
     const datapelanggan={
