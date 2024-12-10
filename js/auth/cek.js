10c4897b137408ed8c1b1873b52d5bb5554e4906
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-11 02:54:04 +0700

    tambah logout button

--- js/auth/cek.js
+++ js/auth/cek.js
@@ -1,4 +1,6 @@
 import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/element.js";
+import {deleteCookie} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/cookie.js";
+import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/url.js";
 
 export function cekDataUser(result){
     console.log(result);
@@ -7,5 +9,9 @@ export function cekDataUser(result){
     }else{
         setInner("user-name",result.data.name);
     }
+}
 
+export function logout(){
+    deleteCookie("login");
+    redirect("/login");
 }
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import {toggleMenu} from "/js/menu/sidebar.js";
 import {onClick} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/element.js";
 import {onHashChange,redirect} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/url.js";
 import {handleHashChange} from "/js/routes/route.js";
-import {cekDataUser} from "/js/auth/cek.js";
+import {cekDataUser,logout} from "/js/auth/cek.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/cookie.js";
 import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/api.js";
 
@@ -15,6 +15,7 @@ if (getCookie("login")){
 
 onClick("burger-menu",toggleMenu);
 onClick("close-menu",toggleMenu);
+onClick("logout-btn",logout);
 
 if (window.location.hash) {
     history.pushState("", document.title, window.location.pathname + window.location.search);
