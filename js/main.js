126cc9e7f0df5b23d5ab0c38f5fcdbf6b6cf2a11
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-10 19:33:12 +0700

    oke sip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import {toggleMenu} from "/js/menu/sidebar.js";
 import {onClick} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/element.js";
+import {onHashChange,getHash} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/url.js";
+import {handleHashChange} from "/js/routes/route.js";
 
 onClick("burger-menu",toggleMenu);
 onClick("close-menu",toggleMenu);
 
+onHashChange(handleHashChange);
