db571bdeb51938a60bcf4e2f87aa4b63d24270b1
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-10 18:48:09 +0700

    close-menu

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import {toggleMenu} from "/js/menu/sidebar.js";
 import {onClick} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/element.js";
 
 onClick("burger-menu",toggleMenu);
+onClick("close-menu",toggleMenu);
