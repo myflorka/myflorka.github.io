8685bb53118890f11ac9aeb2c9d129d15ab7b90e
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-11 12:03:14 +0700

    cobain

--- js/content/lihatclient.js
+++ js/content/lihatclient.js
@@ -1,10 +1,31 @@
 import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.9/api.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.9/cookie.js";
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
 
-export function mainLihatClient(){
-    getJSON("",'login',getCookie('login'),runAfterGetData);
+export async function mainLihatClient(){
+    //menambahkan css
+    if(!isCSSLoaded("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css")){
+        await addCSSInHead("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css");
+    }
+    getJSON("https://asia-southeast2-awangga.cloudfunctions.net/florka/data/pelanggan/all",'login',getCookie('login'),runAfterGetData);
 }
 
-function runAfterGetData(){
-    
+function runAfterGetData(result){
+    if (result.status!==200){
+        Swal.fire(result.data.status, result.data.response, 'warning');
+        return;
+    }
+    const tbody = document.querySelector('.table-container table tbody');
+    result.data.forEach((item, index) => {
+        const row = `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${item.namapelangan}</td>
+                <td>${item.namapasangan}</td>
+                <td>${item.nomorpelanggan}</td>
+                <td>${item.tanggal}</td>
+            </tr>
+        `;
+        tbody.innerHTML += row;
+    });
 }
