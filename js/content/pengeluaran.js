8a59cfd43f432387cdd1ce308a5bb7ac59bc8e73
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-11 16:55:39 +0700

    benerin

--- js/content/pengeluaran.js
+++ js/content/pengeluaran.js
@@ -1,6 +1,6 @@
 import {getValue,onClick,container,onInput,addCSSInHead,isCSSLoaded} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.9/element.js";
-import {validatePhoneNumber} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/validate.js";
 import {postJSON} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/api.js";
+import {getQueryString} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.9/url.js";
 import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.1.8/cookie.js";
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
 
@@ -14,24 +14,21 @@ export async function mainpengeluaranClient(){
     }
     //tombol
     onClick('tombolsubmitpengeluaran',aksiSubmit);
-    onInput('nomorpelanggan',validatePhoneNumber);
     //check kelengkapan
-    onInput('namapelanggan',checkInputs);
-    onInput('namapasangan',checkInputs);
-    onInput('lokasi',checkInputs);
-    onInput('tanggal',checkInputs);
-    onInput('paket',checkInputs);
+    onInput('jenis',checkInputs);
     onInput('harga',checkInputs);
-    onInput('alamat',checkInputs);
-    
+    onInput('objek',checkInputs);
+    onInput('tanggal',checkInputs);
+    onInput('keterangan',checkInputs);
+   
 }
 
 
 function checkInputs() {
-    if (getValue('namapelanggan').trim() !== '' && getValue('nomorpelanggan').trim() !== '' 
-    && getValue('namapasangan').trim() !== '' && getValue('lokasi').trim() !== '' 
-    && getValue('paket').trim() !== '' && getValue('harga').trim() !== '' 
-    && getValue('tanggal').trim() !== '' && getValue('alamat').trim() !== '') {
+    const query = getQueryString(); // Memanggil fungsi
+    if (getValue('jenis').trim() !== '' && getValue('objek').trim() !== '' 
+    && getValue('harga').trim() !== '' && getValue('keterangan').trim() !== '' 
+    && getValue('tanggal').trim() !== '' && query.idpel.trim() !== '' ) {
       container('tombolsubmitclient').disabled = false; // Aktifkan tombol
     } else {
       container('tombolsubmitclient').disabled = true; // Nonaktifkan tombol
@@ -39,22 +36,23 @@ function checkInputs() {
   }
 
 function aksiSubmit(){
-    const datapelanggan={
-        idpelanggan:getValue('idpel'),
+  const query = getQueryString();
+    const datapengeluaran={
+        idpelanggan:query.idpel,
         jenis:getValue('jenis'),
         objek:getValue('objek'),
         harga:getValue('harga'),
         keterangan:getValue('keterangan'),
         tanggal:getValue('tanggal')
     }
-    console.log(datapelanggan); 
-    postJSON("https://asia-southeast2-awangga.cloudfunctions.net/florka/data/pelanggan","login",getCookie("login"),datapelanggan,runafterPostDataPelanggan)
+    console.log(datapengeluaran); 
+    postJSON("https://asia-southeast2-awangga.cloudfunctions.net/florka/data/pengeluaran","login",getCookie("login"),datapelanggan,runafterPostDataPengeluaran)
 }
 
-function runafterPostDataPelanggan(result){
+function runafterPostDataPengeluaran(result){
   console.log(result);
   if (result.status === 200){
-    Swal.fire('Berhasil', 'Data '+result.data.namapelanggan+' sudah dimasukkan dengan ID '+result.data.id, 
+    Swal.fire('Berhasil', 'Data '+result.data.objek+' sudah dimasukkan dengan ID '+result.data.id, 
       'success').then(({ isConfirmed }) => {
         if (isConfirmed) {
           window.location.href = '/';
