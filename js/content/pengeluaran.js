133f4b4eddcd85d883776cf823ac6985e8214f7a
Rolly Maulana Awangga <awangga@gmail.com>
2024-12-11 17:17:18 +0700

    container('tombolsubmitpengeluaran').disabled = true; // Nonaktifkan tombol

--- js/content/pengeluaran.js
+++ js/content/pengeluaran.js
@@ -60,6 +60,7 @@ function runafterPostDataPengeluaran(result){
           setValue('harga','');
           setValue('keterangan','');
           setValue('tanggal','');
+          container('tombolsubmitpengeluaran').disabled = true; // Nonaktifkan tombol
         }
       });
   }else{
